36d1a6873f79fde11fdc70d15b79516a5d8f98ca
Victor <dibiavc@us.ibm.com>
2017-01-17 19:22:49 -0500

    add description to config sample

--- config_sample.js
+++ config_sample.js
@@ -1,19 +1,19 @@
 // Please replace the username and password with your bluemix credentials
 
 
-exports.password = 'xxx-xxx-xxx' ;
-exports.username = 'xxx-xxx-xxx' ;
-exports.STTCustomizationid = 'xxxxxxxxx'
+exports.STTpassword = 'xxx-xxx-xxx' ;   // Speech to text password
+exports.STTusername = 'xxx-xxx-xxx' ;    // Speech to text username
+exports.STTCustomizationid = 'xxxxxxxxx'   // Speech to text Customization id (if you are using a customized language model)
 
 //Credentials for Watson Text to Speech service
 
-exports.TTSPassword = 'xxx-xxx-xxx' ;
+exports.TTSPassword = 'xxx-xxx-xxx' ;   // Text to speech password
 exports.TTSUsername = 'xxx-xxx-xxx' ;
 
 // Credentials for Watson Conversation service
-exports.ConPassword = 'xxxxxx' ;
-exports.ConUsername = 'xxx-xxx-xxx' ;
-exports.ConWorkspace = 'xxx-xxx-xxx';
+exports.ConPassword = 'xxxxxx' ;         // conversation service password
+exports.ConUsername = 'xxx-xxx-xxx' ;   // conversation service username
+exports.ConWorkspace = 'xxx-xxx-xxx';     // Conversation workspace id
 
 // Credentials for Vision Api
 exports.VisionKey = 'xxx-xxx-xxx' ;
